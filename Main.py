20c517d84879ccbe8ca27cc616ce2a2f79c797ec
skulitom <artemskulimovskiy@gmail.com>
2017-02-20 13:39:58 +0000

    second commit

--- Main.py
+++ Main.py
@@ -1,6 +1,15 @@
 def main():
-    fileText = open("robots.mat.txt");
+    readFile = open("robots.mat.txt");
+    outputFile = open("output.txt", 'w');
+    outputFile.truncate();
+    outputFile.write("erumpent\nj1kb4rrbduujb6fqvurrqdbvoc\n");
+    for line in readFile:
+        solveInstance(line,outputFile);
+    readFile.close();
+    outputFile.close();
 
+def solveInstance(line,outputFile):
+    x = 10;
 
 if __name__ == "__main__":
     main();
