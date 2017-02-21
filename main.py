aa488ade4a25d7548b05e604f8453eb114b48c20
skulitom <artemskulimovskiy@gmail.com>
2017-02-21 14:32:31 +0000

    simpler functions

--- main.py
+++ main.py
@@ -9,15 +9,30 @@ def main_algorithm():
     # problems = parser.parse()
 
     # Example of Writing solution
-    sol = Solution(question_number=1)
+    writeSolution(Solution(question_number=1))
+    writeSolution(Solution(question_number=2))
+    # Example Usage of visualization
+    problems = parser.parse()
+    drawSolution(problems[12])
 
+def writeSolution(sol):
     l = list()
     for i in range(0, 10):
         l.append((float(i), float(i+10)))
 
     sol.list_of_coordinates = [l]
     writer.write_solution([sol])
-    # Example Usage of visualization
+
+
+def drawSolution(problem):
+    polygons = list()
+
+    for obstacle in problem.obstacles:
+        polygons.append(visualization.draw_polygon(obstacle.vertices, width=0.1))
+        point = (0.5, 0.5)
+        print(obstacle.in_area(point=point))
+
+    visualization.draw(polygons)
 
 
 if __name__ == "__main__":
