4caae6575bb1a401a4f1175e4477a279650041b1
Justin Kim <justinkim@mishipay.com>
2017-02-23 16:48:05 +0000

    .

--- main.py
+++ main.py
@@ -2,65 +2,30 @@ from models import Graph, Edge
 from utilities import custom_logger, parser, writer
 import logging
 import visualization
+import math
 import copy
 from multiprocessing import Process
 
 
 def main_algorithm():
     problems = parser.parse()
-    pb = problems[30]
+    pb = problems[3]
 
     graph = Graph(pb)
     graph.sort_edges()
     solution_edges = list()
-    connected_edges = list()
 
-    nodes = set()
-    robots = set()
-    pb_robots = set(pb.robots)
+    MST = kruskal(graph, graph.edges)
 
-    # No obstacles:
-    if pb.obstacles is None:
-        print(pb.obstacles)
-        solution_edges.append(kruskal(graph, graph.edges))
-        return
+    pb_robots = copy.deepcopy(pb.robots)
+    while len(pb_robots) > 0:
+        r_i = pb_robots.pop(0)
+        v_i = r_i.vertices
 
-    # for edge in graph.edges:
-    #     # Find the shortest path between robots
-    #     if edge.robots:
-    #         if len(edge.robots) > 1:
-    #             if robots == pb_robots:  # Found all robot-paths
-    #                 continue
-    #             if not set(edge.robots).issubset(robots):
-    #                 for robot in edge.robots:
-    #                     robots.add(robot)
-    #                 solution_edges.append(edge)
-    #         else:
-    #             # Start of the edge is a robot
-    #             if edge.start in graph.robot_vertices and edge.end not in nodes:
-    #                 nodes.add(edge.end)
-    #                 solution_edges.append(edge)
-    #
-    #             elif edge.end in graph.robot_vertices and edge.start in nodes:
-    #                 nodes.add(edge.start)
-    #                 solution_edges.append(edge)
-    #
-    #     else:  # Edge from obstacle to obstacle or boundary
-    #         if not edge.is_boundary:  # non-boundary edge
-    #             nodes.add(edge.start)
-    #             nodes.add(edge.end)
-    #             solution_edges.append(edge)
-    #
-    # node_path_nodes = list(copy.deepcopy(nodes))
-    # while len(node_path_nodes) > 0:
-    #     n_i = node_path_nodes.pop(0)
-    #     for n_d in node_path_nodes:
-    #         for obstacle in pb.obstacles:
-    #             if n_i in obstacle.vertices and n_d in obstacle.vertices:
-    #                 for edge in obstacle.find_path(n_i, n_d):
-    #                     solution_edges.append(Edge(edge[0], edge[1], None))
-
-    solution_edges = kruskal(graph, graph.edges)
+        for r in pb_robots:
+            v_d = r.vertices
+            solution_edges.extend(find_path(v_i, v_d, graph.vertices, MST))
+            print(solution_edges)
     # Visualize is using process (non blocking)
     Process(target=visualization.draw(pb, mst_edges=solution_edges, edges=graph.edges)).start()
 
@@ -92,6 +57,90 @@ def find_set(sets, v1):
     for s in sets:
         if v1 in s:
             return s
+
+
+def find_neighbors(v, edges):
+
+    n = list()
+
+    for edge in edges:
+        if edge.start == v:
+            n.append(edge.end)
+        elif edge.end == v:
+            n.append(edge.start)
+
+    return n
+
+
+def find_path(v_1, v_2, vertices, edges):
+    s = list()
+    dist, prev = dijkstra_path(v_1, vertices, edges)
+
+    u = v_2
+    while prev[u] is not None:
+        s.append(u)
+        u = prev[u]
+    s += [u]
+
+    edges = list()
+    for i in range(0, len(s)):
+        a = s[i]
+
+        if i == len(s) - 1:
+            b = s[0]
+        else:
+            b = s[i + 1]
+
+        edges.append(find_edge(a, b, edges))
+
+    return edges[:-1]
+
+
+def dijkstra_path(v_1, vertices, edges):
+    # Using Dijkstra
+
+    q = set()
+    dist = dict()
+    prev = dict()
+
+    for v in vertices:
+        dist[v] = float('inf')
+        prev[v] = None
+        q.add(v)
+
+    dist[v_1] = 0
+
+    while q:
+        dist_q = {v: dist[v] for v in q}
+
+        u = min(dist_q, key=lambda x: dist_q[x])
+        q.remove(u)
+
+        for v in find_neighbors(u, edges):
+            edge = find_edge(u, v, edges)
+            alt = dist[u] + edge.weight
+            if alt < dist[v]:
+                dist[v] = alt
+                prev[v] = u
+
+    return dist, prev
+
+
+def find_edge(v1, v2, edges):
+    for edge in edges:
+        if edge.start == v1 and edge.end == v2:
+            return edge
+        elif edge.start == v2 and edge.end == v1:
+            return edge
+
+    return Edge(v1, v2, None)
+
+
+def calculate_weight(v1, v2):
+    delta_x = v1[0] - v2[0]
+    delta_y = v1[1] - v2[1]
+
+    return math.sqrt(delta_x * delta_x + delta_y * delta_y)
 
 if __name__ == "__main__":
     custom_logger.start_logger()
--- models/obstacle.py
+++ models/obstacle.py
@@ -32,75 +32,7 @@ class Obstacle(object):
 
         return edges
 
-    def find_neighbors(self, v):
 
-        n = list()
-
-        for edge in self.boundaries:
-            if edge[0] == v:
-                n.append(edge[1])
-            elif edge[1] == v:
-                n.append(edge[0])
-
-        return n
-
-    def find_path(self, v_1, v_2):
-        s = list()
-        dist, prev = self.dijkstra_path(v_1)
-
-        u = v_2
-        while prev[u] is not None:
-            s.append(u)
-            u = prev[u]
-        s += [u]
-
-        edges = list()
-        for i in range(0, len(s)):
-            a = s[i]
-
-            if i == len(s) - 1:
-                b = s[0]
-            else:
-                b = s[i + 1]
-
-            edges.append((a, b))
-
-        return edges[:-1]
-
-    def dijkstra_path(self, v_1):
-        # Using Dijkstra
-
-        q = set()
-        dist = dict()
-        prev = dict()
-
-        for v in self.vertices:
-            dist[v] = float('inf')
-            prev[v] = None
-            q.add(v)
-
-        dist[v_1] = 0
-
-        while q:
-            dist_q = {v: dist[v] for v in q}
-
-            u = min(dist_q, key=lambda x: dist_q[x])
-            q.remove(u)
-
-            for v in self.find_neighbors(u):
-                alt = dist[u] + self.calculate_weight(u, v)
-                if alt < dist[v]:
-                    dist[v] = alt
-                    prev[v] = u
-
-        return dist, prev
-
-    @staticmethod
-    def calculate_weight(v1, v2):
-        delta_x = v1[0] - v2[0]
-        delta_y = v1[1] - v2[1]
-
-        return math.sqrt(delta_x * delta_x + delta_y * delta_y)
 
 
 
